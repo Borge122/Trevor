934421476b4a50e7cc7e662de4c40019835abd1b
Borge122 <gbird1972@hotmail.co.uk>
2020-10-22 22:54:08 +0100

    Update subroutines.py

--- subroutines.py
+++ subroutines.py
@@ -14,7 +14,6 @@ def response(text):
         file.write(f"{datetime.datetime.now().strftime('%Y:%b:%d-%H:%M:%S')} -:- {text}\n")
     return None
 
-requests("Tests")
 def say(text):
     engine.say(text)
     engine.runAndWait()
