a85aac1ab69fa82a918e636b023d8ade5f77b95c
Borge122 <gbird1972@hotmail.co.uk>
2020-10-23 17:01:58 +0100

    Update

--- Calenders/obtainCalender.py
+++ Calenders/obtainCalender.py
@@ -1,6 +1,27 @@
+import datetime
 NAME = "George"
 CALENDER = "Lectures"
+pythonised_calender = []
+
+
+def string_to_datetime(dt):
+    print(dt)
+    return datetime.datetime.strptime(dt, "%Y%m%d:%H%M%S")
+
+
 with open(r"{0}\{1}.ics".format(NAME, CALENDER), "r") as file:
     events = file.read().split("STATUS:CONFIRMED")[1:]
     for event in events:
-        print(event)
+        pythonised_calender.append({})
+        for information in event.split("\n"):
+            if "SUMMARY" in information:
+                pythonised_calender[-1]["SUMMARY"] = information[information.find(":")+1:]
+            elif "DESCRIPTION" in information:
+                pythonised_calender[-1]["DESCRIPTION"] = information[information.find(":")+1:]
+            elif "DTSTART" in information:
+                pythonised_calender[-1]["Start"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
+            elif "DTEND" in information:
+                pythonised_calender[-1]["End"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
+            elif "RRULE" in information:
+                pythonised_calender[-1]["Rule"] = information[information.find(":")+1:]
+print(pythonised_calender)
