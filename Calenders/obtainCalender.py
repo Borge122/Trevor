5f01bf75138c1691369ee9f51176090069ff8842
Borge122 <gbird1972@hotmail.co.uk>
2020-10-23 17:05:28 +0100

    Update

--- Calenders/obtainCalender.py
+++ Calenders/obtainCalender.py
@@ -1,7 +1,4 @@
 import datetime
-NAME = "George"
-CALENDER = "Lectures"
-pythonised_calender = []
 
 
 def string_to_datetime(dt):
@@ -9,19 +6,21 @@ def string_to_datetime(dt):
     return datetime.datetime.strptime(dt, "%Y%m%d:%H%M%S")
 
 
-with open(r"{0}\{1}.ics".format(NAME, CALENDER), "r") as file:
-    events = file.read().split("STATUS:CONFIRMED")[1:]
-    for event in events:
-        pythonised_calender.append({})
-        for information in event.split("\n"):
-            if "SUMMARY" in information:
-                pythonised_calender[-1]["SUMMARY"] = information[information.find(":")+1:]
-            elif "DESCRIPTION" in information:
-                pythonised_calender[-1]["DESCRIPTION"] = information[information.find(":")+1:]
-            elif "DTSTART" in information:
-                pythonised_calender[-1]["Start"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
-            elif "DTEND" in information:
-                pythonised_calender[-1]["End"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
-            elif "RRULE" in information:
-                pythonised_calender[-1]["Rule"] = information[information.find(":")+1:]
-print(pythonised_calender)
+def getcalender(folder, calender_name):
+    pythonised_calender = []
+    with open(r"{0}\{1}.ics".format(folder, calender_name), "r") as file:
+        events = file.read().split("STATUS:CONFIRMED")[1:]
+        for event in events:
+            pythonised_calender.append({})
+            for information in event.split("\n"):
+                if "SUMMARY" in information:
+                    pythonised_calender[-1]["SUMMARY"] = information[information.find(":")+1:]
+                elif "DESCRIPTION" in information:
+                    pythonised_calender[-1]["DESCRIPTION"] = information[information.find(":")+1:]
+                elif "DTSTART" in information:
+                    pythonised_calender[-1]["Start"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
+                elif "DTEND" in information:
+                    pythonised_calender[-1]["End"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
+                elif "RRULE" in information:
+                    pythonised_calender[-1]["Rule"] = information[information.find(":")+1:]
+    return pythonised_calender
--- subroutines.py
+++ subroutines.py
@@ -69,10 +69,35 @@ def get_audio(log=True, lights=True):
     return text
 
 
-def set_light(r,g,b):
-    light.rgb = [r,g,b]
+def set_light(r, g, b):
+    light.rgb = [r, g, b]
 
 
-def refreshLightConnection():
+def refresh_light_connection():
     global light
     light = magichue.Light('192.168.1.47')
+
+
+def string_to_datetime(dt):
+    print(dt)
+    return datetime.datetime.strptime(dt, "%Y%m%d:%H%M%S")
+
+
+def get_calender(folder, calender_name):
+    python_calender = []
+    with open(r"{0}\{1}.ics".format(folder, calender_name), "r") as file:
+        events = file.read().split("STATUS:CONFIRMED")[1:]
+        for event in events:
+            python_calender.append({})
+            for information in event.split("\n"):
+                if "SUMMARY" in information:
+                    python_calender[-1]["SUMMARY"] = information[information.find(":")+1:]
+                elif "DESCRIPTION" in information:
+                    python_calender[-1]["DESCRIPTION"] = information[information.find(":")+1:]
+                elif "DTSTART" in information:
+                    python_calender[-1]["Start"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
+                elif "DTEND" in information:
+                    python_calender[-1]["End"] = string_to_datetime(information[information.find(":")+1:].replace("T", ":").replace("Z", ""))
+                elif "RRULE" in information:
+                    python_calender[-1]["Rule"] = information[information.find(":")+1:]
+    return python_calender
