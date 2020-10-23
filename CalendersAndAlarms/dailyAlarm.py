f1d2ef436362af43542d0b59e6d1e54b749cf8a2
Borge122 <gbird1972@hotmail.co.uk>
2020-10-23 19:09:29 +0100

    Update 2

--- CalendersAndAlarms/dailyAlarm.py
+++ CalendersAndAlarms/dailyAlarm.py
@@ -7,11 +7,10 @@ def obtain(user):
     # #Get Lectures
     calendar = get_calender(user, "Lectures")
     # #Get Tutorials
-    #calendar += get_calender(user, "Tutorials")
+    calendar += get_calender(user, "Tutorials")
     for event in calendar:
-        "print(event)"
-        if todays_date.weekday() == event["Start"].weekday():
-            "print(event)"
-
+        if "Start" in event.keys():
+            if todays_date.weekday() == event["Start"].weekday():
+                print(event)
 obtain("George")
 
--- subroutines.py
+++ subroutines.py
@@ -87,7 +87,6 @@ def get_calender(folder, calender_name):
 
     with open(r"{0}\{1}.ics".format(folder, calender_name), "r") as file:
         events = file.read().split("STATUS:CONFIRMED")[1:]
-        print(events)
         for event in events:
             python_calender.append({})
             for information in event.split("\n"):
