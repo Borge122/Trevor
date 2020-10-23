cb7f1954028edad3f60d35ff3e1db5cbda375a46
Borge122 <gbird1972@hotmail.co.uk>
2020-10-23 16:01:28 +0100

    Update

---
+++ TestCode/SpotifyCode.py
@@ -0,0 +1,6 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+scope = "user-library-read"
